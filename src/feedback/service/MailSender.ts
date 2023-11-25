8526c0e58f1614d8faa14e89c6063327c7708542
achref <achrefbenmbarek123@gmail.com>
2023-11-25 09:43:53 +0100

    using the application password instead of oauth to send emails with gmail(nodemailer)

--- src/feedback/service/MailSender.ts
+++ src/feedback/service/MailSender.ts
@@ -1,39 +1,22 @@
 import nodemailer from 'nodemailer';
-import { google } from 'googleapis';
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-
-const oAuth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URI
-);
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 async function sendMail(feedbackTextContent:string, feedbackHtmlContent:string) {
   try {
-    console.log(process.env.REFRESH_TOKEN)
-    console.log("mail yeh dik 1")
-    const accessToken = await oAuth2Client.getAccessToken();
-
+    const SENDER = process.env.RECIPIENT
+    const RECIPIENT = process.env.SENDER
+    const PASSWORD = process.env.PASSWORD
     const transport = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        type: 'OAuth2',
-        user: process.env.SENDER,
-        clientId: CLIENT_ID,
-        clientSecret: CLIENT_SECRET,
-        refreshToken: REFRESH_TOKEN,
-        accessToken: accessToken,
+        user: SENDER,
+        pass: PASSWORD
       },
     } as nodemailer.TransportOptions);
 
     const mailOptions: nodemailer.SendMailOptions = {
-      from: `SENDER NAME <${process.env.SENDER}>`,
-      to: process.env.RECIPIENT,
+      from: `SENDER NAME <${SENDER}>`,
+      to: RECIPIENT,
       subject: 'feedback',
       text:feedbackTextContent,
       html: feedbackHtmlContent,
@@ -50,4 +33,5 @@ async function sendMail(feedbackTextContent:string, feedbackHtmlContent:string) 
 }
 
 export { sendMail };
+
 
